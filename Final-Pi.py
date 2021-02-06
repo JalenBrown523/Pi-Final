29709711ad818b824d76ec5afd0642152e08db45
Jalen Brown <jmb156@latech.edu>
2021-02-06 15:00:29 -0600

    Update to Final

--- Final-Pi.py
+++ Final-Pi.py
@@ -1,5 +1,7 @@
 from tkinter import *
+import re
 import requests
+# PIL library to import images
 
 class ControlFrame(Frame):
     # The constructor
@@ -21,15 +23,16 @@ class ControlFrame(Frame):
 
         # Creates the Textbox
         ControlFrame.user_input = Entry(self, bg="white", font='kristen 16')
-
-        # Function that will process input from user
-        ControlFrame.user_input.bind("<Return>", self.process)
         ControlFrame.user_input.pack(side=BOTTOM, fill=X)
+        # Initial input is set('focused') on the Textbox
         ControlFrame.user_input.focus()
+
+        # Binds enter key to process input from user
+        ControlFrame.user_input.bind("<Return>", self.process)
 
     def process(self, event):
         # Resets the recepie list when one a word is entered
-        IngredientFrame.myList.delete(0, END)
+        ResultFrame.myList.delete(0, END)
 
         # Take the input from the input line and sets them all to lower case
         action = ControlFrame.user_input.get().lower()
@@ -45,7 +48,7 @@ class ControlFrame(Frame):
         # Create list of recipe items
         ControlFrame.Recipes = [Recipe(recipeJSON) for recipeJSON in responseJSON["results"]]
 
-        # If responsejson is an empy string(ex. no response) then chage the text of the instruction lable
+        # If responsejson is an empTy string(ex. no response) then change the text of the instruction label
         # Handles invalid input from the user
         if (responseJSON == []):
             print("Search Error: Invalid ingredient/No results")
@@ -56,7 +59,7 @@ class ControlFrame(Frame):
             ControlFrame.instructLabel.config(text="What Ingredients do you have?", fg="black",
                                               font="helvetica 18 bold")
 
-        # Ddd recipes to GUI
+        # Adds recipes to the list
         for recipe in ControlFrame.Recipes:
             rFrame.addRecipe(recipe)
 
@@ -79,48 +82,62 @@ class Recipe():
         return self.title
 
 
-class IngredientFrame(Frame):
+class ResultFrame(Frame):
     def __init__(self, parent):
         # Call the constructor in the superclass
         Frame.__init__(self, parent)
 
+    def listRecipes(self):
+        rFrame.pack_forget()
+        cFrame.pack(side=TOP, expand=1, fill=BOTH)
+        ResultFrame.Frame.pack_forget()
+        ResultFrame.Frame.pack(side=TOP , fill=X)
+        ResultFrame.BackButton.pack_forget()
+
+
     def setupGUI(self):
         self.pack(side=TOP, expand=1, fill=BOTH)
 
-        IngredientFrame.RecipeInfo = Text(self, state=DISABLED)
-        IngredientFrame.RecipeInfo.pack(side=TOP, fill=BOTH)
+        # Creates the bottom window
+        ResultFrame.Frame = Frame(window)
+        ResultFrame.Frame.pack(side=TOP, fill=X)
 
-        IngredientFrame.instructLabel = Label(self, text="Your list of recipes")
-        IngredientFrame.instructLabel.pack(anchor=N)
+        ResultFrame.RecipeInfo = Text(self, state=DISABLED, wrap=WORD, font='verdana')
+
+        ResultFrame.instructLabel = Label(ResultFrame.Frame, text="Your list of recipes")
+        ResultFrame.instructLabel.pack(anchor=N, pady=5)
 
         # Setup scroll bar
-        IngredientFrame.scroll = Scrollbar(window)
-        IngredientFrame.scroll.pack(side=RIGHT, fill=Y)
+        ResultFrame.scroll = Scrollbar(window)
+        ResultFrame.scroll.pack(side=RIGHT, fill=Y)
 
         # Create list
-        IngredientFrame.myList = Listbox(window)
-        IngredientFrame.myList.pack(side=TOP, expand=1, fill=BOTH)
-        IngredientFrame.myList.bind('<<ListboxSelect>>', self.expandRecipe)
+        ResultFrame.myList = Listbox(window, font='Times')
+        ResultFrame.myList.pack(side=TOP, expand=1, fill=BOTH)
+        ResultFrame.myList.bind('<<ListboxSelect>>', self.expandRecipe)
 
-        # Link scroll bar to listbox
-        IngredientFrame.myList.config(yscrollcommand=IngredientFrame.scroll.set)
-        IngredientFrame.scroll.config(command=IngredientFrame.myList.yview)
+        # Link scroll option to listbox
+        ResultFrame.myList.config(yscrollcommand=ResultFrame.scroll.set)
+        ResultFrame.scroll.config(command=ResultFrame.myList.yview)
 
     def addRecipe(self, Recipe):
-        IngredientFrame.myList.insert(END,
+        ResultFrame.myList.insert(END,
                                   f"{Recipe.title}  |  Likes: {Recipe.likes}  |  Missing Ingredients: {Recipe.ingMiss}")
 
     def expandRecipe(self, event):
         # Runs if a recipe is selected
-        if (IngredientFrame.myList.curselection()):
-            recipe = ControlFrame.Recipes[IngredientFrame.myList.curselection()[0]]
+        if (ResultFrame.myList.curselection()):
+            recipe = ControlFrame.Recipes[ResultFrame.myList.curselection()[0]]
+
             # TODO Check if this recipe is already displayed
             response = requests.get(f"https://api.spoonacular.com/recipes/{recipe.id}/information?apiKey={api_key}")
             responseJSON = response.json()
-            IngredientFrame.RecipeInfo.config(state=NORMAL)
-            IngredientFrame.RecipeInfo.delete("1.0", END)
+            ResultFrame.RecipeInfo.config(state=NORMAL)
+
+            # Clear textbox
+            ResultFrame.RecipeInfo.delete("1.0", END)
             sumarry = responseJSON["summary"]
-            IngredientFrame.RecipeInfo.insert(END, f"{recipe.title}\n\n{sumarry}")
+            ResultFrame.RecipeInfo.insert(END, f"{recipe.title}\n\n{sumarry}")
 
 
 ##################################################################################
@@ -140,7 +157,7 @@ cFrame = ControlFrame(window)
 cFrame.setupGUI()
 
 # Create the recipe display frame inside the window
-rFrame = IngredientFrame(window)
+rFrame = ResultFrame(window)
 rFrame.setupGUI()
 
 # Wait for the window to close
